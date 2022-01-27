d82354e19bd0f79672781b489a2eef33cfa1348a
gml9812 <hee.jun.yun@gmail.com>
2022-01-27 15:48:52 +0900

    chore: eslint 설정 수정

--- src/pages/Example1/index.jsx
+++ src/pages/Example1/index.jsx
@@ -1,1 +1,21 @@
-import { someUtilFunc1 } from './utils';
+import { getExchangeRate } from './utils';
+
+export function ExchangeResultBox({ from, to, amount }) {
+  const timestamp = getExchangeRate('timestamp');
+
+  return (
+    <ResultBox>
+      <h1>
+        {`${to} `}
+        {calculateExchangedAmount({ from, to, amount })}
+      </h1>
+      <div>{`기준일: ${timestampToDate(timestamp)}`}</div>
+    </ResultBox>
+  );
+}
+
+ExchangeResultBox.propTypes = {
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+};
