820fac40c30326bb08c23ea04d713f88eec90fbf
jambottle <jaewon.walks@gmail.com>
2022-02-07 05:20:30 +0900

    feat: Router.tsx 생성 및 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from 'styles/GlobalStyle';
-import App from './App';
+import Router from 'Router';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root'),
 );
